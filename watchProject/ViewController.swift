b3c7192b39fe2286a445ac59bde6b3ee3097ce18
Braden Bowdish <braden@bradens-macbook-air.local>
2018-01-22 20:47:34 -0500

    Initial Commit

---
+++ watchProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchProject
+//
+//  Created by Braden Bowdish on 1/22/18.
+//  Copyright © 2018 Braden Bowdish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
