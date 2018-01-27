d64fd7e81610d9d5444e79d54e0068822db93924
Braden Bowdish <braden@bradens-macbook-air.local>
2018-01-27 14:14:49 -0500

    iOS: Created 2 buttons. One label. Leftmost button changes text from Hello World to Goodbye world. Rightmost button changes text to a random number.

--- watchProject/ViewController.swift
+++ watchProject/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var theLabel: UILabel!
+    
+    
+    /// Get a random number between two bounds. 
+    func getRandom(lowerBound: UInt32, upperBound: UInt32) -> Int{
+        let uniformInt: UInt32 = (upperBound - lowerBound) + 1
+        return Int(arc4random_uniform(uniformInt) + lowerBound);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonAClicked(){
+        let goodbye: String = "Goodbye world."
+        let hello: String = "Hello world."
+        if (theLabel.text == goodbye){
+            theLabel.text = hello
+            theLabel.textColor = UIColor.blue
+        }
+        else {
+            theLabel.text = goodbye
+            theLabel.textColor = UIColor.red
+        }
+    }
+    
+    @IBAction func buttonBClicked() {
+
+        theLabel.text = "\(getRandom(lowerBound: 1, upperBound: 10))"
+        
+//        theLabel.text = "\(getRandom(lowerBound: 0, upperBound: 10))"
+        
+        theLabel.textColor = UIColor.green
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
